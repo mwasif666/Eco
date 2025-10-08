6ac61e9c31b2386b1e1c4a0e67d4a98f10b929fe
Muhammad Wasif <mwasif66625426@gmail.com>
2025-10-08 19:34:48 +0500

    footer remove

--- src/pages/homes/home-2.tsx
+++ src/pages/homes/home-2.tsx
@@ -108,7 +108,7 @@ const HomeTwoMain = () => {
           </main>
 
           {/* footer area */}
-          <FooterTwo />
+          {/* <FooterTwo /> */}
           {/* footer area */}
         </div>
       </div>
